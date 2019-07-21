d2893e11c442dfddf6c8d9f79cc27939b668de3e
Александр Манолов <manolov@pyxis.ripcm.com>
2019-07-22 02:11:10 +0300

    add

--- scripts/run_orthofinder.sh
+++ scripts/run_orthofinder.sh
@@ -1,8 +1,7 @@
-orthofinder=$1
-threads=$2
+threads=$1
 orthofinder -t $threads -a $threads -og -f faa
 mkdir -p tmp
 find faa -name 'Orthogroups.txt' -exec cp {} tmp \; 
 find faa -name 'Orthogroups.csv' -exec cp {} tmp \; 
-perl scripts/namedGroups2table.pl tmp/Orthogroups.txt
-mv ortho_table_names.txt ortho_table.txt tmp
+#perl scripts/namedGroups2table.pl tmp/Orthogroups.txt
+#mv ortho_table_names.txt ortho_table.txt tmp
