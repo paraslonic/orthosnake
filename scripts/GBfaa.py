94fa4529b9cd90659936132047a1f7a91d17349a
paraslonic <paraslonic@gmail.com>
2020-04-28 11:37:04 +0300

    names in faa fix

--- scripts/GBfaa.py
+++ scripts/GBfaa.py
@@ -1,6 +1,6 @@
 from Bio import SeqIO
 from argparse import ArgumentParser
-from os.path import splitext
+import os
 
 parser = ArgumentParser()
 parser.add_argument('-gb', type=str, default=None, help='GenBank file')
@@ -10,17 +10,19 @@ args = parser.parse_args()
 
 file = args.gb
 gbs = [seq for seq in SeqIO.parse(open(file), 'genbank')]
-name = splitext(file)[0]
+name = os.path.basename(file)
+name = os.path.splitext(name)[0]
 id_ = 1
 for gb in gbs:
     contig = gb.name
     feats = [feat for feat in gb.features if feat.type=='CDS']
     for feat in feats:
-        print('>' + name + '|' + 
-                str(id_) + '|' + 
-                feat.qualifiers['product'][0].replace(' ', '_') + '|' + 
-                contig + '|' + str(feat.location.start) + '|' + 
+        print('>' + name + '|' +
+                str(id_) + '|' +
+                feat.qualifiers['product'][0].replace(' ', '_') + '|' +
+                contig + '|' + str(feat.location.start) + '|' +
                 str(feat.location.end))
 
         print(feat.qualifiers['translation'][0])
         id_ += 1
+
