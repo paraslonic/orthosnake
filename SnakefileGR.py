bfa6dfe45064a8e7a58a3bdc3a34a60d1059fc65
Nikolay Popov <npopov@tux.ripcm.com>
2021-09-10 14:42:29 +0300

    Main functions of pipilene is done

--- SnakefileGR.py
+++ SnakefileGR.py
@@ -13,8 +13,12 @@ ogs = list(ogs[0])
 
 rule all:
     #input: 'tmp/ogs_nt_fasta/', 'tmp/ogs_aa_fasta/'
-    input: expand('tmp/ogs_aa_aligned_fasta/{og}.fasta', og=ogs)
-        
+    #input: expand('tmp/map_files/{og}.txt', og=ogs)
+    input: 'Results/geneRaxResults'
+    
+    
+# Добавить фильтр по количеству последовательностей
+
 rule create_og_list:
     input: 'Results/Orthogroups.txt'
     output: "tmp/list_of_ogs.txt"
@@ -31,7 +35,7 @@ rule catGenomes:
     shell: 
         '''
         cat tmp/genome_fasta_aa/*.fasta > {output[0]}
-        cat tmp/genome_fasta_aa/*fasta > {output[1]}
+        cat tmp/genome_fasta_nt/*fasta > {output[1]}
         '''
 rule splitToOg_nt:
     input: 'Results/Orthogroups.txt', 'tmp/all_genomes_nt.fasta', "tmp/list_of_ogs.txt"
@@ -47,7 +51,7 @@ rule splitToOg_aa:
         '''
         python scripts/splitToOg.py {input[0]} {input[1]} {input[2]} tmp/ogs_aa_fasta/
         '''
-
+        
 rule align_amino_acids:
     input: 'tmp/ogs_aa_fasta/{og}.fasta'
     output: 'tmp/ogs_aa_aligned_fasta/{og}.fasta'
@@ -56,3 +60,36 @@ rule align_amino_acids:
         '''
         muscle -in {input} -out {output}
         '''
+rule align_nt:
+    input: 
+        nt = 'tmp/ogs_nt_fasta/{og}.fasta',
+        aa = 'tmp/ogs_aa_aligned_fasta/{og}.fasta'
+    output: 
+        'Results/ogs_nt_aligned_fasta/{og}.fasta'
+    shell:
+        '''
+        python scripts/GapsFromAA.py {input.nt} {input.aa} {output}
+        '''
+        
+
+rule make_maps_file:
+    input: 'Results/ogs_nt_aligned_fasta/{og}.fasta'
+    output : 'tmp/map_files/{og}.txt'
+    shell:
+        '''
+        python scripts/crMapFile.py {input} {output}
+        '''
+rule make_families_file:
+    input:
+        files = expand('tmp/map_files/{og}.txt', og=ogs)
+    output:
+        'tmp/familiesFile.txt'
+    shell:
+        '''
+        python scripts/crFamiliesFile.py Results/ogs_nt_aligned_fasta/ tmp/map_files/ {output}
+        '''
+rule run_generax:
+    input: 'tmp/familiesFile.txt'
+    output: directory('Results/geneRaxResults')
+    threads: 10
+    shell:'mpiexec -np 10 helpers/GeneRax/build/bin/generax -f {input} -s random -p {output}'
