12961d3a7a00c1958241bb46964e5b00e05ce39b
Nikolay Popov <npopov@tux.ripcm.com>
2021-09-22 16:35:35 +0300

    Добавил копирование gene_count в папку Results

--- scripts/run_orthofinder.sh
+++ scripts/run_orthofinder.sh
@@ -2,3 +2,4 @@ threads=$1
 orthofinder -t $threads -a $threads -og -f tmp/genome_fasta_aa
 mkdir -p tmp
 find tmp/genome_fasta_aa -name 'Orthogroups.txt' -exec cp {} Results \; 
+find tmp/genome_fasta_aa -name 'Orthogroups.GeneCount.tsv' -exec cp {} Results \;
